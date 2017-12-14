7d0cd290d676721218c24d31ba1d0a06aa317059
Nakul Goud <nakulgoud@gmail.com>
2017-12-14 08:16:01 +0530

    refs NAK-0001 Array rotate

--- pract/dsa/PYTHON/array/rotate.py
+++ pract/dsa/PYTHON/array/rotate.py
@@ -9,7 +9,7 @@ def gcd(a, b):
         return a
     return gcd(b, a%b)
 
-def left_rotate(arr, rotate_by):
+def left_rotate_method1(arr, rotate_by):
     for i in range(0, gcd(len(arr), rotate_by)):
         temp = arr[i]
         j = i
@@ -26,6 +26,26 @@ def left_rotate(arr, rotate_by):
 
 arr = [1,2,3,4,5,6,7,8,9,10,11,12]
 rotate_by = 7
-left_rotate(arr, rotate_by)
+left_rotate_method1(arr, rotate_by)
 print arr
 
+#========================================
+def rverseArray(arr, start, end):
+    while (start < end):
+        temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start += 1
+        end = end-1
+ 
+# Function to left rotate arr[] of size n by d
+def left_rotate_method2(arr, d):
+    n = len(arr)
+    rverseArray(arr, 0, d-1)
+    rverseArray(arr, d, n-1)
+    rverseArray(arr, 0, n-1)
+
+arr = [1,2,3,4,5,6,7]
+rotate_by = 3
+left_rotate_method2(arr, rotate_by)
+print arr
